b51973a6609be1b1befc7bf4b650f44ceb7b4614
Max Plank <darkhan.r4industry@gmail.com>
2019-07-28 14:42:13 +0600

    componenets updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <div className="container">Hello World!</div>
-      </React.Fragment>
+      <Router>
+        <Navbar />
+        <br />
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+      </Router>
     );
   }
 }
